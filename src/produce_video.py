0c3260e4d61d4e66cd32d31dfb402cafd0128d5c
Johan Martinsson <martinsson@users.noreply.github.com>
2023-07-17 23:20:39 +0200

    - normalize audio - increase intro and intro fade time - deduplicate - add png output

--- src/produce_video.py
+++ src/produce_video.py
@@ -13,7 +13,10 @@ from yaml import load, dump
 from moviepy.editor import VideoFileClip, TextClip, ImageClip, CompositeVideoClip, concatenate_videoclips, AudioClip
 from moviepy.video import fx
 from moviepy.video.fx import crop, resize
-from moviepy.audio.fx import audio_fadeout
+from moviepy.audio.fx import audio_fadeout, audio_normalize
+
+
+
 
 
 """
@@ -62,6 +65,7 @@ class VideoInfo:
         self.conf_file_contents = conf_file_contents
 
         video_name = filename.removesuffix(".yml")
+        self.video_name = video_name
         self.output_file = video_name + ".mp4"
         self.full_path_on_disk = video_name + ".mkv"
 
@@ -69,43 +73,55 @@ class VideoInfo:
         self.title = conf_file_contents.title
         self.presenter = presenter.title()
 
+    def output_thumbnail(self):
+        return self.video_name + ".png"
+
 
 def do_it_all(video_info: VideoInfo) -> None:
 
     clips = video_info.conf_file_contents.clip
     start = clips[0].start_seconds()
     print("start " + str(start))
-    max_duration_main_clip = 300
-    end_time = start + max_duration_main_clip
+
+    # end time of video
+    # max_duration_main_clip = 10
+    # end_time = start + max_duration_main_clip
     end_time = clips[0].stop_seconds()
+
     presentation_clip: VideoClip = VideoFileClip(video_info.full_path_on_disk, target_resolution=(1080, 1920))\
         .subclip(start, end_time)
 
-    intro = intro_clip(video_info, presentation_clip.size)
+    intro_duration = 7
+    intro = intro_clip(video_info, intro_duration)
 
-    full_audio = compose_audio(intro, presentation_clip)
+    full_audio = compose_audio(intro_duration, presentation_clip)
 
     # speed trick: use compose for fade and avoid it for speed for the rest of the video
     first_part = concatenate_videoclips([intro.crossfadeout(2), presentation_clip.crossfadein(2)], method="compose")\
-        .set_duration(7)\
+        .set_duration(intro_duration)\
         .set_audio(full_audio)
 
-    full_video = concatenate_videoclips([first_part, presentation_clip.set_start(first_part.end)]).set_audio(full_audio)
+    full_video: VideoClip = concatenate_videoclips([first_part, presentation_clip.set_start(first_part.end)]).set_audio(full_audio)
 
+    full_video.resize(.5).save_frame(video_info.output_thumbnail(), t=0.1)
     full_video.write_videofile(video_info.output_file, fps=25, codec='libx264')  # Many options...
 
 
-def compose_audio(intro, presentation_clip):
+def compose_audio(intro_duration: int, presentation_clip: VideoClip):
     sound_file_name = f"{resource_dir}/music/bensound-onceagain.mp3"
-    fade_duration = 2
-    music_clip = AudioFileClip(sound_file_name).subclip(0, intro.duration + fade_duration)
+    fade_duration = 4
+    music_clip = AudioFileClip(sound_file_name).subclip(0, intro_duration + fade_duration)
+
     faded = audio_fadeout.audio_fadeout(music_clip, fade_duration)
-    presentation_audio = presentation_clip.audio.set_start(intro.duration)
-    full_audio = CompositeAudioClip([faded, presentation_audio])
+    presentation_audio = presentation_clip.audio.set_start(intro_duration)
+    all_clips = [faded, presentation_audio]
+    normalized_clips = [audio_normalize.audio_normalize(normalized) for normalized in all_clips]
+    full_audio = CompositeAudioClip(normalized_clips)
+
     return full_audio
 
 
-def intro_clip(video_info: VideoInfo, size):
+def intro_clip(video_info: VideoInfo, intro_duration):
     logo = f"{resource_dir}/Logo/03-AlpesCraft_Couleurs-M.png"
     logo_clip = ImageClip(logo).set_position(('center', 0.2), relative=True)
 
@@ -113,16 +129,15 @@ def intro_clip(video_info: VideoInfo, size):
     image_clip = ImageClip(background_image)
     cropped_image = crop.crop(image_clip, x1=0, y1=0, width=1920)
     resized = resize.resize(cropped_image, newsize=(1920, 1080))
-    title = (TextClip(video_info.title, fontsize=60, color='white', stroke_width=3)
+    title = (TextClip(video_info.title, fontsize=52, color='white', stroke_width=3)
                   .set_position(('center', 0.5), relative=True))
     presenter_name = (TextClip(video_info.presenter, fontsize=45, color='white', stroke_width=2)
                       .set_position(('center', 0.60), relative=True))
 
     intro = CompositeVideoClip([resized, logo_clip, title, presenter_name])
 
-    title_duration = 5
 
-    return intro.set_duration(title_duration)
+    return intro.set_duration(intro_duration)
 
 
 
