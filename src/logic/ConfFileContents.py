55702ea14c567be77af799d28153ed54ca334caf
Johan Martinsson <martinsson@users.noreply.github.com>
2024-01-09 00:01:26 +0100

    simplify and fix problem with config file loading

--- src/logic/ConfFileContents.py
+++ src/logic/ConfFileContents.py
@@ -13,7 +13,7 @@ class ConfFileContents:
 
     @classmethod
     def from_dict(cls, param: dict):
-        sound_file_spec = param["sound_file"]
+        sound_file_spec = param["sound"]
         sound_file = SoundFile.from_sound_file_spec(sound_file_spec["file_name"], sound_file_spec["start"])
         clips = [ClipSection.from_clip_spec(x["start"], x["stop"]) for x in param["clip"]]
         return cls(
