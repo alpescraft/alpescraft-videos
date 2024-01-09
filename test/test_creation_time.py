dba7b10fe7a65df8d57b859d3960d367a00b0eef
Johan Martinsson <martinsson@users.noreply.github.com>
2024-01-09 08:57:53 +0100

    add exifdependency and doc

--- test/test_creation_time.py
+++ test/test_creation_time.py
@@ -1,6 +1,9 @@
+import datetime
 from os.path import getctime
 
 import pytest
+from moviepy.video.VideoClip import VideoClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 def get_offset_seconds(reference_file_ctime: float, reference_file_offset: int, file_ctime: float) -> float:
@@ -23,3 +26,31 @@ class TestCase:
         # result = getctime("./test_creation_time.py")
         offset_seconds = get_offset_seconds(reference_file_ctime, reference_file_offset, file_ctime)
         assert offset_seconds == expected_offset_seconds
+
+    def test_time(self) -> None:
+        ctime = getctime("/Users/johan/Downloads/Vocal 001.m4a")
+        assert datetime.datetime.fromtimestamp(ctime) == 0
+
+    @pytest.mark.skip
+    def test_assemble_videos(self):
+        target_resolution = (1080, 1920)
+        reference_file_offset = 1
+        presentation_clip: VideoClip = VideoFileClip("/Volumes/files/alpescraft/ht/ht-jan-1-speaker.mp4", target_resolution=target_resolution) \
+            .subclip(reference_file_offset, 4)
+        slides_ctime = getctime("/Volumes/files/alpescraft/ht/ht-jan-1-slides.mp4")
+        reference_ctime = getctime("/Volumes/files/alpescraft/ht/ht-jan-1-speaker.mp4")
+        slides_clip: VideoClip = VideoFileClip("/Volumes/files/alpescraft/ht/ht-jan-1-slides.mp4", target_resolution=target_resolution) \
+            .subclip(get_offset_seconds(reference_ctime, 1, slides_ctime), 4)
+
+
+"""
+exiftool -G1 -s -a -time:all ht-jan-01-slides.mkv
+[System]        FileModifyDate                  : 2024:01:08 22:37:26+01:00
+[System]        FileAccessDate                  : 2024:01:08 22:37:26+01:00
+[System]        FileInodeChangeDate             : 2024:01:08 22:37:26+01:00
+exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01-slides.mkv
+exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01-speaker.mp4
+[QuickTime]     CreateDate                      : 2024:01:08 21:37:20
+exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01.m4a
+[QuickTime]     CreateDate                      : 2024:01:08 21:37:23
+"""
