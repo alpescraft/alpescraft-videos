59813cea36fbaa440751ca41b31565b5ea55a7d7
Johan Martinsson <martinsson@users.noreply.github.com>
2024-01-10 00:36:03 +0100

    create function for getting the real starting date

--- test/test_creation_time.py
+++ test/test_creation_time.py
@@ -5,6 +5,9 @@ import pytest
 from moviepy.video.VideoClip import VideoClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
+AUDIO_FILE_EXAMPLE = "/Users/johan/Downloads/Vocal 001.m4a"
+MKV_FILE_EXAMPLE = "/Users/johan/Movies/2024-01-09 19-07-11.mkv"
+
 
 def get_offset_seconds(reference_file_ctime: float, reference_file_offset: int, file_ctime: float) -> float:
     return reference_file_ctime + reference_file_offset - file_ctime
@@ -27,9 +30,19 @@ class TestCase:
         offset_seconds = get_offset_seconds(reference_file_ctime, reference_file_offset, file_ctime)
         assert offset_seconds == expected_offset_seconds
 
+    def test_gettime_exif(self) -> None:
+        absolute_file_path = AUDIO_FILE_EXAMPLE
+        created_date = get_created_date(absolute_file_path)
+        assert created_date == datetime.datetime(2024, 1,8, 21,37,23)
+
+        created_date = get_created_date(MKV_FILE_EXAMPLE)
+        assert created_date == datetime.datetime(2024, 1,9, 19,7,22, tzinfo=datetime.timezone(datetime.timedelta(seconds=3600))) # + 1
+
+
+
     def test_time(self) -> None:
-        ctime = getctime("/Users/johan/Downloads/Vocal 001.m4a")
-        assert datetime.datetime.fromtimestamp(ctime) == 0
+        ctime = getctime(AUDIO_FILE_EXAMPLE)
+        assert datetime.datetime.fromtimestamp(ctime) == datetime.datetime(2024, 1, 9, 0, 34, 55, 234462)
 
     @pytest.mark.skip
     def test_assemble_videos(self):
@@ -42,15 +55,28 @@ class TestCase:
         slides_clip: VideoClip = VideoFileClip("/Volumes/files/alpescraft/ht/ht-jan-1-slides.mp4", target_resolution=target_resolution) \
             .subclip(get_offset_seconds(reference_ctime, 1, slides_ctime), 4)
 
+def get_created_date(absolute_file_path) -> datetime:
+    from exiftool import ExifToolHelper
+
+    with ExifToolHelper() as et:
+        if absolute_file_path.endswith(".mkv"):
+            tags = et.get_tags([absolute_file_path], ["FileModifyDate"])
+            return datetime.datetime.strptime( tags[0]["File:FileModifyDate"],'%Y:%m:%d %H:%M:%S%z')
+        tags = et.get_tags([absolute_file_path], ["CreateDate"])
+        return datetime.datetime.strptime( tags[0]["QuickTime:CreateDate"],'%Y:%m:%d %H:%M:%S')
+
+
 
 """
 exiftool -G1 -s -a -time:all ht-jan-01-slides.mkv
 [System]        FileModifyDate                  : 2024:01:08 22:37:26+01:00
 [System]        FileAccessDate                  : 2024:01:08 22:37:26+01:00
 [System]        FileInodeChangeDate             : 2024:01:08 22:37:26+01:00
+
 exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01-slides.mkv
 exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01-speaker.mp4
 [QuickTime]     CreateDate                      : 2024:01:08 21:37:20
+
 exiftool -G1 -s -a -QuickTime:CreateDate ht-jan-01.m4a
 [QuickTime]     CreateDate                      : 2024:01:08 21:37:23
 """
