1eac4b35dcfd52d1a87224757cbbe5c029d9100c
Johan Martinsson <martinsson@users.noreply.github.com>
2024-01-21 18:36:06 +0100

    extracted methods

--- src/logic_v2/collate_video.py
+++ src/logic_v2/collate_video.py
@@ -20,32 +20,61 @@ def collate_main_part(video_info: PresentationInfo, presenter_slide_ratio: Tuple
     start, length = video_collage_info.get_start_length()
 
     presentation_file_path = video_collage_info.presentation_file_path
-    presentation_clip = make_video_clip(presentation_file_path, .5, start, length)
-    w, h = 1920*.5, 1080*.5
-    fade_duration = 2
-    presentation_clip = crop.crop(presentation_clip, width=w*.5, height=h, x_center=w / fade_duration, y_center=h / fade_duration)
+    target_resolution = video_collage_info.target_resolution
 
-    track_file = video_collage_info.slides_file
+    slides_clip = create_slides_clip(length, presentation_file_path, presenter_slide_ratio, start,
+                                     video_collage_info.slides_file)
+
+    sound_clip = create_sound_clip(length, presentation_file_path, start, video_collage_info.sound_file)
+
+    presentation_composition = create_presentation_clip(length, presentation_file_path, slides_clip, start,
+                                                        target_resolution, video_info)
+
+    intro_duration = 2
+    fade_duration = 2
+    intro = intro_clip(video_info, intro_duration)
+
+    final_audio = compose_audio(fade_duration, intro_duration, sound_clip, video_info)
+
+    final_clip = blend_intro_and_main_clip(fade_duration, intro, intro_duration, presentation_composition)
+
+    return final_clip.set_audio(final_audio)
+
+
+def create_presentation_clip(length, presentation_file_path, slides_clip, start, target_resolution, video_info):
+    presentation_clip = make_video_clip(presentation_file_path, .5, start, length)
+    w, h = 1920 * .5, 1080 * .5
+    presentation_clip = crop.crop(presentation_clip, width=w * .5, height=h, x_center=w / 2, y_center=h / 2)
+    presentation_composition = compose_main_video(length, presentation_clip, slides_clip, target_resolution, video_info)
+    return presentation_composition
+
+
+def create_slides_clip(length, presentation_file_path, presenter_slide_ratio, start, track_file):
     slides_file_path = track_file.file_name
     slides_relative_start = get_relative_start_time(presentation_file_path, slides_file_path,
                                                     start) + track_file.extra_offset
     slides_clip: VideoClip = make_video_clip(slides_file_path, presenter_slide_ratio[1], slides_relative_start, length)
+    return slides_clip
 
-    track_file = video_collage_info.sound_file
+
+def create_sound_clip(length, presentation_file_path, start, track_file):
     sound_file_path = track_file.file_name
     sound_relative_start = get_relative_start_time(presentation_file_path, sound_file_path,
                                                    start) + track_file.extra_offset
     sound_clip = AudioFileClip(sound_file_path).subclip(sound_relative_start, sound_relative_start + length)
-
     print("start ", start)
     print("sound_start", sound_relative_start)
-    # exit(0)
+    return sound_clip
+
+
+def compose_main_video(length, presentation_clip, slides_clip, target_resolution, video_info):
     logo_250x600 = crop.crop(ImageClip(video_info.logo), y1=175, y2=600 - 175)
     log_100x240 = resize.resize(logo_250x600, .4)
     logo_clip_above_speaker = log_100x240.set_position((120, 135))
-    location_clip = (TextClip("GRENOBLE", fontsize=60, color='white', stroke_color='grey', stroke_width=2)).set_position((100, 1080*.75+40))
-    background_color = ColorClip((1920, 1080), color=(110, 20, 86))
-
+    location_clip = (
+        TextClip("GRENOBLE", fontsize=60, color='white', stroke_color='grey', stroke_width=2)).set_position(
+        (100, 1080 * .75 + 40))
+    background_color = ColorClip(target_resolution, color=(110, 20, 86))
     presentation_clips = [
         background_color,
         slides_clip.set_position(("right", "center")),
@@ -53,23 +82,25 @@ def collate_main_part(video_info: PresentationInfo, presenter_slide_ratio: Tuple
         logo_clip_above_speaker,
         location_clip,
     ]
-    target_resolution = video_collage_info.target_resolution
     presentation_composition = (CompositeVideoClip(presentation_clips, size=target_resolution)
                                 .subclip(0, length))
+    return presentation_composition
 
-    intro_duration = 2
-    intro = intro_clip(video_info, intro_duration)
 
+def blend_intro_and_main_clip(fade_duration, intro, intro_duration, presentation_composition):
+    final_clip = CompositeVideoClip(
+        [transitions.crossfadeout(intro, fade_duration), transitions.crossfadein(presentation_composition.set_start(
+            intro_duration), fade_duration)])
+    return final_clip
+
+
+def compose_audio(fade_duration, intro_duration, sound_clip, video_info):
     jingle = AudioFileClip(video_info.jingle).subclip(0, intro_duration + fade_duration)
     from moviepy.audio.fx import audio_fadeout, audio_normalize
     audio_clips = [audio_fadeout.audio_fadeout(jingle, fade_duration), sound_clip.set_start(intro_duration)]
     # normalized_audio_clips = [audio_normalize.audio_normalize(normalized) for normalized in [jingle, sound_clip]]
     final_audio = CompositeAudioClip(audio_clips)
-
-    final_clip = CompositeVideoClip([transitions.crossfadeout(intro, fade_duration), transitions.crossfadein(presentation_composition.set_start(
-        intro_duration), fade_duration)])
-
-    return final_clip.set_audio(final_audio)
+    return final_audio
 
 
 def make_video_clip(presentation_file_path, ratio, start, length):
