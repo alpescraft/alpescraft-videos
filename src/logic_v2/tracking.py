5da79997607f2d11df7063733538626dcb179fda
Johan Martinsson <martinsson@users.noreply.github.com>
2024-06-24 23:40:06 +0200

    tracking with a box of the right size

--- src/logic_v2/tracking.py
+++ src/logic_v2/tracking.py
@@ -3,17 +3,31 @@ from moviepy.video.VideoClip import VideoClip
 
 
 def tracking_crop_of_presenter(presentation_clip: VideoClip):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    # Read the input image
-    img = cv2.imread('test.jpg')
-    # Convert into grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    # Display the output
-    cv2.imshow('img', img)
-    cv2.waitKey()
+    first_image = presentation_clip.get_frame(0).copy()
+    # img = cv2.imread('test.jpg')
+    zone = detect_person(first_image)
+    tracker = cv2.TrackerCSRT.create()
+    tracker.init(first_image, zone)
+
     def track_presenter(frame):
-        return frame
+        success, box = tracker.update(frame)
+        (x, y, w, h) = box
+        box_center = (x + w // 2, y + h // 2)
+        box_scaled_to_480x810 = (box_center[0] - 240, box_center[1] - 405, 480, 810)
+
+        (x, y, w, h) = box_scaled_to_480x810
+        newframe= frame.copy()
+        cv2.rectangle(newframe, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        return newframe
     return presentation_clip.fl_image(track_presenter)
+
+
+def detect_person(image):
+    face_cascade = cv2.CascadeClassifier('logic_v2/tracking/haarcascade_upperbody.xml')
+
+    # Convert into grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    return faces[0]
--- src/produce_tracking_example.py
+++ src/produce_tracking_example.py
@@ -1,34 +1,33 @@
 import cv2
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-from logic_v2.tracking import tracking_crop_of_presenter
+from logic_v2.tracking import tracking_crop_of_presenter, detect_person
 
 
-def detect_person(first_image):
-
-    initial_detection_image = first_image.copy()
-    # Convert into grayscale
-    gray = cv2.cvtColor(initial_detection_image, cv2.COLOR_BGR2GRAY)
-    # # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    return faces[0]
-
-
-if __name__ == "__main__":
+def do_it():
     original = VideoFileClip(
         "/Users/johan/Documents/alpescraft videos 2024/sessions/cyrille martraire/speaker.mp4",
         target_resolution=(1920, 1080))
-
-    face_cascade = cv2.CascadeClassifier('logic_v2/tracking/haarcascade_upperbody.xml')
     # Read the input image
     first_image = original.get_frame(0).copy()
-
     # img = cv2.imread('test.jpg')
-    (x, y, w, h) = detect_person(first_image)
+    zone = detect_person(first_image)
+    visualize_detection(first_image, zone)
+
+
+def visualize_detection(first_image, zone):
+    (x, y, w, h) = zone
     cv2.rectangle(first_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     # Display the output
     cv2.imshow('img', first_image)
     cv2.waitKey()
 
-    # tracked_clip = tracking_crop_of_presenter(original)
-    # tracked_clip.subclip(0, 3).write_videofile("tracked_clip.mp4", codec="libx264", fps=24)
+
+if __name__ == "__main__":
+    # do_it()
+    original = VideoFileClip(
+        "/Users/johan/Documents/alpescraft videos 2024/sessions/cyrille martraire/speaker.mp4",
+        target_resolution=(1920, 1080))
+
+    tracked_clip = tracking_crop_of_presenter(original)
+    tracked_clip.subclip(0, 3).write_videofile("tracked_clip.mp4", codec="libx264", fps=24)
